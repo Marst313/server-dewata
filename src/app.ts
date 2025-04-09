3ba01b423854742f57feceb844a6668482cd7f4e
Marst313 <dlegoinyoman1@gmail.com>
2025-04-09 12:42:55 +0700

    update origin cors

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.ORIGIN,
+    origin: [process.env.ORIGIN as string, 'http://localhost:5173'],
     credentials: true,
   })
 );
