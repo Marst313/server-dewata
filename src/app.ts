ae42140f89061f8a594ac922030b3de4463c5647
Marst313 <dlegoinyoman1@gmail.com>
2025-04-12 20:19:24 +0700

    update

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import express, { NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-import session from 'express-session';
-import dotenv from 'dotenv';
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import session from "express-session";
+import dotenv from "dotenv";
 
-import authRouter from './route/authRoutes';
-import pelangganRouter from './route/pelangganRoutes';
-import { handleError } from './utils/responseHandler';
+import authRouter from "./route/authRoutes";
+import pelangganRouter from "./route/pelangganRoutes";
+import { handleError } from "./utils/responseHandler";
 
 dotenv.config();
 
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [process.env.ORIGIN as string, 'http://localhost:5173', 'https://dewata.vercel.app'],
+    origin: [process.env.ORIGIN as string, "http://localhost:5174", "https://dewata.vercel.app"],
     credentials: true,
   })
 );
@@ -24,22 +24,22 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === 'production',
+      secure: process.env.NODE_ENV === "production",
       httpOnly: true,
       maxAge: 1000 * 60 * 60,
     },
   })
 );
 
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.use('/auth', authRouter);
-app.use('/api/v1/pelanggan', pelangganRouter);
+app.use("/auth", authRouter);
+app.use("/api/v1/pelanggan", pelangganRouter);
 
-app.all('*', (req: Request, res: Response, next: NextFunction) => {
-  return next(handleError(res, 404, 'Route not found'));
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  return next(handleError(res, 404, "Route not found"));
 });
 
 export default app;
